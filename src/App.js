8a7703bd2a1c1d060a67c64acf6d5e0fccb2ba74
JuanitaAgudelo <agujuanis7@gmail.com>
2021-10-08 09:16:44 -0500

    Modificación archivo App

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import FooterComponent from './shared/components/footer/FooterComponent';
 import BarraBusqueda from './Usuario-roles/Barra-busqueda/BarraBusqueda';
 import TablaUsuarios from './Usuario-roles/Tabla-usuarios/TablaUsuarios';
 import InformeVentas from "./InformeVentas/InformeVentas";
+import ProductosPage from './productos/ProductosPage';
+import ListadoProductosPage from './productos/ListadoProductosPage';
+import Ventas from './Ventas/Ventas';
+
 function App() {
   return (
     <Router>
       <HeaderComponent/>
       <Switch>
         <Route path="/" exact>
-          <h1>Home</h1>
+          <h1>PAGINA PRINCIPAL</h1>
         </Route>
         <Route path="/InformeVentas" exact>
           <InformeVentas></InformeVentas>
         </Route>
         <Route path="/Ventas" exact>
-          <h1>Ventas</h1>
           <Link to="/InformeVentas" className="btn btn-primary">Consultar Ventas</Link>
+          <Ventas></Ventas>
         </Route>
-        <Route path="/Productos" exact>
-          <h1>Productos</h1>
+        <Route path="/Productos" exact>          
+          <ProductosPage></ProductosPage>
+        </Route>
+        <Route path="/ListadoProductos" exact>          
+          <ListadoProductosPage></ListadoProductosPage>
         </Route>
         <Route path="/Usuarios" exact>
           <BarraBusqueda/>
