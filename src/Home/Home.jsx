d1fc59aa013f01257b147cbc06af44cb905813d5
Juanita Agudelo <90080604+JuanitaAgudelo@users.noreply.github.com>
2021-10-24 21:28:10 -0500

    Restricción de acceso interfaces según el rol

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,30 +9,70 @@ import apiBaseUrl from "../shared/Utils/Api";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Home(){
-    const getUsuario=()=>{
-        axios.get(`${apiBaseUrl}/add-usuario`).then((response)=>{
-            
+    
+    const[botonActivoUsuario, setBotonActivoUsuario]=useState();
+    const[botonActivoVentas, setBotonActivoVentas]=useState();
+    const[botonActivoProductos, setBotonActivoProductos]=useState();
+    
+    const {correo}=useParams()
+    
+    const[rol, setRol]=useState({ usuario: [] });
+
+    const getRol=()=>{
+        axios.get(`${apiBaseUrl}/get-informacion-usuario/${correo}`).then((response)=>{
+            setRol(response.data[0])  
         }
     );}
+    useEffect(() => {
+        getRol();
+    }, []);
     
+    useEffect(() => {
+        if(rol.rol=="Vendedor"){
+            setBotonActivoUsuario(false)
+            setBotonActivoVentas(true)
+            setBotonActivoProductos(true)
+            alert(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+            console.log(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+            
+        }else if(rol.rol=="Administrador"){
+            
+            setBotonActivoUsuario(true)
+            setBotonActivoVentas(true)
+            setBotonActivoProductos(true)
+            alert(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+            console.log(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+
+        }else if(rol.rol=="Pendiente"){
+            setBotonActivoUsuario(false)
+            setBotonActivoVentas(false)
+            setBotonActivoProductos(false)
+            alert(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+            console.log(botonActivoProductos, botonActivoUsuario, botonActivoVentas)
+
+        }
+    }, []);
+  
     return(
         <Fragment>
         <br/>
         <div className="containerC">
             <div className="card">
-                <NavLink to="/Ventas" ><img src={ventas} alt="Ventas"/></NavLink>
+                <img src={ventas} alt="Ventas"/>
                 <h3>Ventas</h3>
+                <button className="btn btn-info mx-2" type="button" disabled={!botonActivoVentas} ><NavLink to="/Productos" >Productos</NavLink></button>
             </div>
 
             <div className="card">
-                <NavLink to="/Productos" ><img src={productos} alt="Productos"/></NavLink>
+                 <img src={productos} alt="Productos"/>
                 <h3>Productos</h3>
+                <button className="btn btn-info mx-2" type="button" disabled={!botonActivoProductos}><NavLink to="/Productos" >Productos</NavLink></button>
             </div>
 
             <div className="card">
-                
-                <NavLink to="/Usuarios" ><img src={roles} alt="Usuarios"/></NavLink> 
+                 <img src={roles} alt="Usuarios" disabled={true}/>
                 <h3>Usuarios</h3> 
+                <button className="btn btn-info mx-2" type="button" disabled={!botonActivoUsuario}><NavLink to="/Productos" >Productos</NavLink></button> 
             </div> 
         </div>
         <br/>
